62c1568f2f5d645ef315b2cca5a4bee2cf2b1ed1
Prashanth D. Rao <pchengi@nsc.liu.se>
2024-07-12 13:37:54 +0200

    set LANG=C.UTF-8

--- compile.sh
+++ compile.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /mnt
-source /usr/local/src/nscjekyllsetup/rubyenv
+source /usr/local/src/nscjekyllsetup/rubyenv $1
+export LANG=C.UTF-8
 jekyll build;
 exit $?;
--- presetup.sh
+++ presetup.sh
@@ -2,4 +2,5 @@
 
 # Some prerequisites
 export 'RBENV_ROOT'=/usr/local/src/rbenv
+echo "export LANG=C.UTF-8" >>/etc/bashrc
 echo "export PATH=$RBENV_ROOT/bin:$PATH" >>/etc/bashrc
